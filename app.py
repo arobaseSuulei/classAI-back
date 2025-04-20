7869e96919f82c7e22acdfcfe3f7a5508c85212d
arobaseSuulei <souleymanediallo2004@outlook.com>
2025-04-20 20:16:39 +0100

    deploy

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from transcript import main as transcript_main
 
 app = Flask(__name__)
 
+
+url = "https://pxyqknxfvimxdcmplbff.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB4eXFrbnhmdmlteGRjbXBsYmZmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyOTMwMzg2MiwiZXhwIjoyMDQ0ODc5ODYyfQ.01dUZFDUgwG4NHijeblvIPIB-fBWZ5i_ch_BR2Kdl1w"
+deepgram_api_key = "f68dadf1fecb5a4a6f77e053ead1425995776351"
+
 @app.route('/')
 def index():
     try:
